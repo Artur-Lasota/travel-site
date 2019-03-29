e830b11c5dbb2abf904fdeb4848ee3b47222af84
Artur <manchox931@gmail.com>
2019-03-29 14:42:28 +0100

    reorganise gul

--- gulpfile.js
+++ gulpfile.js
@@ -1,32 +1,8 @@
-var gulp = require('gulp'),
-watch = require('gulp-watch'),
-postcss = require('gulp-postcss'),
-autoprefixer = require('autoprefixer'),
-cssvars = require('postcss-simple-vars'),
-nested = require('postcss-nested'),
-cssImport = require('postcss-import');
+require('./app/gulp/tasks/styles.js');
+require('./app/gulp/tasks/watch.js');
+
+var gulp = require('gulp');
 
 gulp.task('default', function(){
     console.log("Gulp task created");
 });
-
- gulp.task('html', function(){
-     console.log("something useful with html");
- });
-
- gulp.task('styles', function(){
-     return gulp.src('./app/assets/styles/styles.css')
-        .pipe(postcss([cssImport, cssvars, nested, autoprefixer]))
-        .pipe(gulp.dest('./app/temp/styles'));
-});
-
-gulp.task('watch', function(){
-    watch('./app/index.html', function(){
-        gulp.start('html')
-    });
-
-    watch('./app/assets/styles/**/*.css', function(){
-        gulp.start('styles');
-    })
-});
-
